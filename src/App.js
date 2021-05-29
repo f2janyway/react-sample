9a7e42026bfc8096309150ec8aa84facdd71bd73
jyg41 <f2janyway@gmail.com>
2021-05-29 21:24:34 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import axios from 'axios';
+function Movie({ title, imageUrl, contents }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <img src={imageUrl} alt={title}/>
+      <div className ="container">
+      <h4><b>{title}</b></h4>
+      <p>{contents}</p>
+      </div>
     </div>
+
   );
+}
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: [],
+    };
+  }
+  call = async () => {
+    axios.get("https://yts.mx/api/v2/list_movies.json").then(response => {
+      this.setState({
+        movies: response.data.data.movies
+      });
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  componentDidMount() {
+    this.call()
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div class="wrapper">
+        {
+          this.state.movies.map((movie) => {
+            return <Movie title={movie.title} imageUrl={movie.large_cover_image} contents={movie.summary} />
+          })
+        }
+
+      </div>
+    );
+  }
+}
+function App() {
+
+  return <Board />
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
