b55ad5a6d6385d3952eec80f330544066b674870
jyg41 <f2janyway@gmail.com>
2021-05-31 20:44:28 +0900

    remove viewport

--- src/App.js
+++ src/App.js
@@ -22,11 +22,6 @@ class Board extends React.Component {
     this.state = {
       movies: [],
     };
-    const meta = document.createElement('meta');
-    meta.name = "viewport";
-    meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover";
-    document.getElementsByTagName('head')[0].appendChild(meta);
-  
   }
   call = async () => {
     axios.get("https://yts.mx/api/v2/list_movies.json?quality=3D").then(response => {
